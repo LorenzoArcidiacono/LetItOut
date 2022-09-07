e9e31ebb98729167b8233337c28b05da191e7c54
Lorenzo Arcidiacono <arci0066@gmail.com>
2022-09-07 20:52:35 +0200

    first commit

---
+++ JS/graphic.js
@@ -0,0 +1,67 @@
+
+let messages;
+
+// Open/Close the single card view
+function openCard(id) {
+    if(messages == null) {
+        messages = JSON.parse(localStorage.getItem('messages'));
+        console.log(`messages: ${messages}`);
+    }
+    let element = messages.messages.find(element => element.id == id)
+    $(".overlay-title").text(element.title);
+    $(".overlay-text").text(element.text);
+    $("#overlay").fadeIn();
+    $("#overlay").css("display", "flex")
+}
+
+function closeCard() {
+    $("#overlay").fadeOut(500);
+    setTimeout(() => {
+        $("#overlay").css("display", "none");
+    }, 600);
+}
+
+function openWriteCard(){
+    // $(".overlay-title").text("Prova");
+    // $(".overlay-text").text(
+    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mattis urna volutpat, congue leout, dictum tellus. Vivamus ac arcu ac dui tempor facilisis. Etiam sit amet mollis mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ultricies, purus eget fringilla iaculis, metus orci placerat purus, quis."
+    // );
+    $("#overlay-new-card").fadeIn();
+    $("#overlay-new-card").css("display", "flex")
+}
+
+function closeWriteCard() {
+    $("#overlay-new-card").fadeOut(500);
+    setTimeout(() => {
+        $("#overlay-new-card").css("display", "none");
+    }, 600);
+}
+
+
+// TODO: numero di colonne in base alla dimensione dello schermo
+function createGrid(totalCardsCount) {
+    let width = $(".MainArea").width();
+    let rowCount = Math.round(totalCardsCount/3)+1;
+    let colCount = Math.floor(width/272);
+    console.log(colCount);
+    var container = document.createElement('div');
+    container.id = "card-grid";
+    container.className = "container-grid";
+
+    for (var i = 0; i < rowCount; i++) {
+        var row = document.createElement('div');
+        row.className = "row";
+        row.id = "row" + i;
+
+        for (var j = 0; j < colCount; j++) {
+            var box = document.createElement('div');
+            box.className = 'col';
+            box.id = 'col'+j;
+            row.appendChild(box);
+        }
+
+        container.appendChild(row);
+    }
+    $(".MainArea").append(container);
+    return container;
+}
---
+++ JS/messages.js
@@ -0,0 +1,98 @@
+const BIN_ID = "6318c12b5c146d63ca92601c";
+const MASTER_KEY = "$2b$10$rwQp/SJS4uB0ZTzBw8L5DuNqbw4ZxFvVvzKz91Nxs28n82CkvYgqy";
+
+let messagesJSON;
+let response;
+
+function saveMessage() {
+    let title = $(".title-input").val();
+    $(".title-input").val('');
+    let text = $(".text-input").val();
+    $(".text-input").val('');
+    let id = Math.random().toString(36).slice(2, 12);
+    console.log(`${id}:${title}: ${text}`);
+    closeWriteCard();
+
+    let jsondata = {
+        id: id,
+        title: title,
+        text: text
+    }
+
+    messagesJSON.messages.push(jsondata);
+    // response.record.messages = messagesJSON.messages
+    console.log(response);
+    
+    updateJsonBin();
+    
+}
+
+function retriveJsonData() {
+    // $.ajax({url: "https://api.npoint.io/c4b4a76ba83b56aa77d3", success: function(result){
+    //     // console.log(JSON.stringify(result));
+    //     messagesJSON = result
+    //     localStorage.setItem('messages', JSON.stringify(messagesJSON));
+    //     createCards();
+    //   }});
+
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            console.log(req.responseText);
+            messagesJSON = JSON.parse(req.responseText);
+            response = JSON.parse(req.responseText);
+            console.log(messagesJSON);
+            localStorage.setItem('messages', JSON.stringify(messagesJSON));
+            createCards();
+        }
+    };
+
+    req.open("GET", "https://api.jsonbin.io/v3/b/" + BIN_ID + "/latest?meta=false", true);
+    req.setRequestHeader("X-Master-Key", MASTER_KEY);
+    req.setRequestHeader("X-Bin-Meta", "false");
+    req.send();
+}
+
+function updateJsonBin() {
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+            console.log(req.responseText);
+        }
+    };
+
+    req.open("PUT", "https://api.jsonbin.io/v3/b/"+BIN_ID, true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("X-Master-Key", MASTER_KEY);
+    req.send(JSON.stringify(messagesJSON));
+}
+
+async function createCards() {
+    let card = `<div class="card" onclick="openCard('$ID')"><h2>$TITLE</h2></div>`
+    if(messagesJSON == undefined) {
+        setTimeout(() => {
+            createCards();
+        }, 400);
+    }
+    else{
+        await createGrid(messagesJSON.messages.length);
+        let i = 0, j=0;
+        let width = $(".MainArea").width();
+        // 272 px = 17rem -> card width
+        let colCount = Math.floor(width/272);
+        messagesJSON.messages.forEach(element => {
+            console.log(element.title);
+            let next = card.replace('$ID',element.id).replace('$TITLE',element.title);
+            console.log(next);
+            $(`.container-grid #row${i} #col${j}`).append(next);
+            j++;
+            if(j == colCount){
+                j = 0;
+                i++;
+            }
+        });
+    }
+}
+
